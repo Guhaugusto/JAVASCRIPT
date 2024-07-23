f8f7cacd5eb48b23f167a5b60ee1b47be36d0967
Guhaugusto <gustavogvfontes2004@gmail.com>
2024-07-23 11:54:43 -0300

    condições

---
+++ ex08/index.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60) {
+    console.log(`voê ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com sinto de segurança!`)
